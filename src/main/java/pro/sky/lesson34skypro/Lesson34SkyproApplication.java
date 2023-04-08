3f284f89f9bfacbaf12d1de52ab22c894f50ea34
MakTub <7andreyru@mail.ru>
2023-04-08 22:45:53 +0500

    ДЗ к уроку 34

---
+++ src/main/java/pro/sky/lesson34skypro/Lesson34SkyproApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.lesson34skypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson34SkyproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson34SkyproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/lesson34skypro/Lesson34SkyproApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.lesson34skypro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson34SkyproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
